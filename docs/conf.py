8986fbf5dc0344b5f4a454a7769096d45cc2a965
Jack Lord <jack.lord@opendataservices.coop>
2017-09-12 10:55:58 +0100

    add jsonschema Sphinx extensions

--- docs/conf.py
+++ docs/conf.py
@@ -20,6 +20,11 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
+import sys
+import os
+import shlex
+import pathlib
+
 from recommonmark.transform import AutoStructify
 from recommonmark.parser import CommonMarkParser
 
@@ -32,7 +37,7 @@ from recommonmark.parser import CommonMarkParser
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = []
+extensions = ['sphinxcontrib.jsonschema']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']

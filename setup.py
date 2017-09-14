6f8ee993b172952e40fc517d07ff608dd41c5d2f
Ben Webb <bjwebb67@googlemail.com>
2017-09-14 16:48:17 +0100

    Install sphinxcontrib-jsonschema from setup.py to use correct setuptools

---
+++ setup.py
@@ -0,0 +1,2 @@
+import subprocess
+subprocess.call(['pip', 'install', '-e', 'git+https://github.com/OpenDataServices/sphinxcontrib-jsonschema.git@b7332e8fd5c788ee59b983396102ddc38967a3d0#egg=sphinxcontrib_jsonschema'])
